1097e72d86c9727ce92f1ddef5c9fd9d196a7196
natsuozawa <natsuozawa@outlook.com>
2018-10-01 15:06:08 +0900

    Add automatic switching functionality to lights

--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
 import './index.scss';
 
+const start = document.getElementById('start');
+const red = document.getElementById('red');
+const yellow = document.getElementById('yellow');
+const green = document.getElementById('green');
+
+const brighten = (elem) => {
+  elem.style.filter = 'brightness(1)';
+}
+
+const darken = (elem) => {
+  elem.style.filter = 'brightness(0.5)';
+}
+
+const eventListener = () => {
+  start.removeEventListener('click', eventListener); //remove this event listener until all jobs are done here
+
+  brighten(red);
+  new Promise(resolve => setTimeout(() => resolve(darken(red)), 1500)
+  ).then(() => {
+    brighten(yellow);
+    return new Promise(resolve => setTimeout(() => resolve(darken(yellow)), 1500));
+  }).then(() => {
+    brighten(green);
+    return new Promise(resolve => setTimeout(() => resolve(darken(green)), 1500));
+  }).then(() => {
+    start.addEventListener('click', eventListener); //readd this event listener since all jobs are done
+  });
+};
+
+start.addEventListener('click', eventListener); //add event listener by default
