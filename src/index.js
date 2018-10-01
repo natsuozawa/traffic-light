845fb5c4094b89493b51bc9d2af65861af91de70
natsuozawa <natsuozawa@outlook.com>
2018-10-01 21:19:16 +0900

    Fix the order of lights

--- src/index.js
+++ src/index.js
@@ -16,14 +16,14 @@ const darken = (elem) => {
 const eventListener = () => {
   start.removeEventListener('click', eventListener); //remove this event listener until all jobs are done here
 
-  brighten(red);
-  new Promise(resolve => setTimeout(() => resolve(darken(red)), 1500)
+  brighten(green);
+  new Promise(resolve => setTimeout(() => resolve(darken(green)), 1500)
   ).then(() => {
     brighten(yellow);
     return new Promise(resolve => setTimeout(() => resolve(darken(yellow)), 1500));
   }).then(() => {
-    brighten(green);
-    return new Promise(resolve => setTimeout(() => resolve(darken(green)), 1500));
+    brighten(red);
+    return new Promise(resolve => setTimeout(() => resolve(darken(red)), 1500));
   }).then(() => {
     start.addEventListener('click', eventListener); //readd this event listener since all jobs are done
   });
